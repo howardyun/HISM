0c1635b40cf70bad2a51e47291c4ef36245eecfa
bupt-Jayce-upperclassman <xlkeepmoving1314@gmail.com>
2021-09-21 17:56:14 +0800

    用户增改查

--- src/test/java/com/HISM/backfront/DemoApplicationTests.java
+++ src/test/java/com/HISM/backfront/DemoApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class DemoApplicationTests {
 
@@ -14,11 +16,48 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
     }
-    @Test
-    void testMybatis(){
-        User user=new User("123456","123","123","123",12,"man","test,test");
-        userMapper.insertUser(user);
 
+
+    @Test
+    void insertUserTest(){
+        try{
+            User user=new User("16","123","123","123",12,"man","test,test");
+            userMapper.insertUser(user);
+        }catch (Exception e){
+            System.out.println("该用户已注册");
+        }
+    }
+
+    @Test
+    void updateUserTest(){
+        try{
+            User user=new User("99","123","bbc","123",19,"man","test,test");
+            userMapper.updateUser(user);
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("该用户不存在,无法更新数据");
+        }
+    }
+
+    @Test
+    void queryUserbyNameTest(){
+        List<User> userList = userMapper.queryUserbyName("bbc");
+        if(userList.isEmpty()){
+            System.out.println("该用户不存在");
+        }else{
+            userList.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void queryUserbyIdTest(){
+        List<User> userList = userMapper.queryUserbyId("16");
+        if(userList.isEmpty()){
+            System.out.println("该用户不存在");
+        }else{
+            User user = userList.get(0);
+            System.out.println(user.toString());
+        }
     }
 
 }
