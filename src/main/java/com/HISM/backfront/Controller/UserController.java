19f7132273a2d6eb921f59d20dd1e1db3de3dc25
云绍轩 <1740010798@qq.com>
2021-10-06 23:48:25 +0800

    some change

--- src/main/java/com/HISM/backfront/Controller/UserController.java
+++ src/main/java/com/HISM/backfront/Controller/UserController.java
@@ -9,10 +9,7 @@ import com.HISM.backfront.domain.Follower;
 import com.HISM.backfront.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -157,7 +154,7 @@ public class UserController {
         return myResult;
     }
 
-    @PostMapping("/changeAvatar")
+    @PutMapping("/changeAvatar")
 
     @ApiOperation("修改头像")
 
